b402cc941c1f2356c9a7b392f84e9ce4bb890b3a
Jeremiah Chienda <jeremiahchienda@gmail.com>
2020-08-15 21:54:44 +0200

    initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,64 @@
+<script>
+  export let segment;
+</script>
+
+<style>
+  nav {
+    position: absolute;
+    margin-top: 7%;
+    font-weight: 300;
+    padding: 0 1em;
+    left: 5%;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  /* clearfix */
+  ul::after {
+    content: "";
+    display: block;
+    clear: both;
+  }
+
+  li {
+    display: block;
+    float: left;
+  }
+
+  [aria-current] {
+    position: relative;
+    display: inline-block;
+  }
+
+  a {
+    text-decoration: none;
+    padding: 1em 2em;
+    display: block;
+    font-weight: bold;
+    text-transform: uppercase;
+    color: var(--bg-yellow);
+  }
+</style>
+
+<nav style="--bg-yellow=var(--bg-yellow)">
+  <ul>
+    <li>
+      <a aria-current={segment === undefined ? 'page' : undefined} href=".">
+        preview
+      </a>
+    </li>
+    <li>
+      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
+        sample
+      </a>
+    </li>
+    <li>
+      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
+        about
+      </a>
+    </li>
+  </ul>
+</nav>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+</script>
+
+<style>
+  main {
+    position: relative;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  :global(:root) {
+    --bg-yellow: #f4b653;
+    --bg-red: #bd3b3b;
+  }
+</style>
+
+<Nav {segment} />
+
+<main>
+  <slot />
+</main>
